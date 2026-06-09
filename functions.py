85a2e64ed62a1c755dd316c820e5a0c8d985689d
Abigail <a.arriagada.aravena@gmail.com>
2026-06-09 12:52:55 -0400

    Avanzando...

---
+++ functions.py
@@ -0,0 +1,5 @@
+def saludar(nombre, lugar, carrera):
+    print(f"Hola {nombre}, vives en {lugar} y estudias la carrera {carrera} en Duoc UC")       
+
+saludar("Abby", "Santiago", "Analista programador")
+
---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+os.system("cls")
+
+print("====BIBLIOTECA====")
+print("1. Agregar libros")
+print("2. Mostrar libros")
+print("")
+print("")
+print("")
+print("")
