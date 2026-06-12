61673a651ef1c8f373ce067c30b53ce2bc2802d9
Abigail <a.arriagada.aravena@gmail.com>
2026-06-12 00:07:33 -0400

    Avance actividad

--- functions.py
+++ functions.py
@@ -1,5 +1,49 @@
-def saludar(nombre, lugar, carrera):
-    print(f"Hola {nombre}, vives en {lugar} y estudias la carrera {carrera} en Duoc UC")       
+libros = [
+    {"autor": "Gabriel García Márquez", "titulo": "Cien años de soledad", "disponibilidad": True},
+    {"autor": "Isabel Allende", "titulo": "La casa de los espíritus", "disponibilidad": True},
+    {"autor": "Jorge Luis Borges", "titulo": "Ficciones", "disponibilidad": True},
+    {"autor": "Mario Vargas Llosa", "titulo": "La ciudad y los perros", "disponibilidad": True},
+    {"autor": "Julio Cortázar", "titulo": "Rayuela", "disponibilidad": True},
+    {"autor": "Freida McFadden", "titulo": "La asistenta", "disponibilidad": True},
+    {"autor": "María Ignacia Urzúa", "titulo": "Una promesa de cien años", "disponibilidad": True},
+    {"autor": "Francisca Solar", "titulo": "El buzón de las impuras", "disponibilidad": True},
+    {"autor": "T.J Klune", "titulo": "La casa en el mar más azul", "disponibilidad": True},
+    {"autor": "Alice Kellen", "titulo": "El mapa de los anhelos", "disponibilidad": True},
+    {"autor": "Edgar Allan Poe ", "titulo": "Narraciones extraordinarias", "disponibilidad": True}
+    
+]
 
-saludar("Abby", "Santiago", "Analista programador")
+def agregar_libro(nombre_libro, autor):
+    libro_nuevo = {"autor": autor, "titulo": nombre_libro, "disponibilidad": True}
+    libros.append(libro_nuevo)
 
+
+def mostrar_libros_registrados(libros):
+    contador = 0
+    for libro in libros:
+        contador = contador + 1
+        if libro["disponibilidad"] == True:
+            estado = "Disponible"
+        else:
+            estado = "Prestado"
+
+        print(f"{contador} - {libro['titulo']} - {libro['autor']} | {estado}")
+
+
+def modificar_libro(indice, nuevo_titulo, nuevo_autor):
+    libros[indice]["titulo"] = nuevo_titulo
+    libros[indice]["autor"] = nuevo_autor
+
+def buscar_titulo_numero(nombre):
+    contador = 0
+    encontrado = False
+    for libro in libros:
+        contador += 1
+        if nombre.lower() in libro["titulo"].lower():
+            print(f"{contador} - {libro['titulo']} - {libro['autor']}")
+            encontrado = True
+    
+    if encontrado == False:
+        print("No se encontró ningún libro con ese título")
+
+    return encontrado
--- main.py
+++ main.py
@@ -1,10 +1,76 @@
 import os
-os.system("cls")
+from functions import mostrar_libros_registrados, libros, agregar_libro, modificar_libro, buscar_titulo_numero
+os.system("clear")
 
-print("====BIBLIOTECA====")
-print("1. Agregar libros")
-print("2. Mostrar libros")
-print("")
-print("")
-print("")
-print("")
+
+menu = True
+
+while menu:
+    os.system("clear")
+    print("====BIBLIOTECA====")
+    print("1. Agregar libro")
+    print("2. Mostrar libros")
+    print("3. Buscar libro")
+    print("4. Prestar libro")
+    print("5. Devolver libro")
+    print("6. Eliminar libro")
+    print("7. Modificar libro")
+    print("8. Mostrar libro")
+    print("9. Salir")
+    
+
+    try:
+
+        opcion = int(input("Elija una opción:\n"))
+
+        if opcion == 1:
+            print("1. Agregar libro")
+            nombre_libro = input("Ingrese el título del libro:\n").title()
+            autor = input("Ingrese el nombre completo del autor:\n").title()
+            agregar_libro(nombre_libro, autor)
+
+
+        elif opcion == 2:
+            print("2. Mostrar libros")
+            mostrar_libros_registrados(libros)
+            
+        elif opcion == 3:
+            print("3. Buscar libro")
+
+        elif opcion == 4:
+            print("4. Prestar libro")
+
+        elif opcion == 5:
+            print("5. Devolver libro")
+
+        elif opcion == 6:
+            print("6. Eliminar libro")
+
+        elif opcion == 7:
+            print("7. Modificar libro")
+            libro_buscado = input("¿Cuál es el título del libro que desea modificar?\n").title()
+            validador = buscar_titulo_numero(libro_buscado)
+
+            if validador == True:
+
+                indice_libro = int(input("De los libros que están en la lista, indíque el número del título que desea modificar:\n"))
+                nombre_libro = input("Modifique el titulo del libro:\n").capitalize()
+                autor = input("Modifique el nombre del autor:\n").capitalize()
+                modificar_libro(indice_libro-1, nombre_libro, autor)
+            else:
+                print("Vuelva a intentarlo con un título existente por favor.")
+        elif opcion == 8:
+            print("Mostrar libro")
+
+        elif opcion == 9:
+            print("9. Salir")
+            break
+        else:
+            print("Opción no válida")
+
+
+    except Exception as e:
+        print(f"Error real: {e}")
+    
+    
+
